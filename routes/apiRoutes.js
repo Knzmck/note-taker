c6737b248051af1649898010d60118a27e4e0d30
Mackenzie Schutz <mshoots64@gmail.com>
2020-05-30 16:47:45 -0500

    app.post route working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,55 +1,50 @@
 const util = require("util");
 const fs = require("fs");
-const fsp = require('fs').promises;
 const path = require('path');
 
 
 module.exports = function (app) {
     // app.get to read the db.json file and return saved notes as JSON
-
+    // uploads existing notes
     app.get("/api/notes", function (req, res) {
         fs.readFile('db/db.json', 'utf8', function (err, data) {
             if (err) throw err
-            // console.log(data);
-            // console.log(JSON.parse(data));
-            res.send(JSON.parse(data));
+            res.send((JSON.parse(data)));
         }
         );
     })
     app.post("/api/notes", function (req, res) {
-        // console.log(req.body);
+        // Reads file before posting data
         fs.readFile('db/db.json', 'utf8', function (err, data) {
             if (err) throw err;
-            // console.log(data);
-            // console.log(JSON.parse(data));
             const noteArray = JSON.parse(data);
             noteArray.push(req.body);
-            // console.log(noteArray)
+            // writes new notes in an array in db file
             fs.writeFile('db/db.json', JSON.stringify(noteArray), function (err, data) {
                 if (err) throw err;
                 res.send(data);
-            })
-
+            });
         });
 
     });
-
+    // deletes notes 
     app.delete("/api/notes/:id", (req, res) => {
-        fs.readFile('db/db.json', 'utf8', function (err, data) {
-            if (err) throw err;
-            const noteArray = JSON.parse(data);
-            let deletedNote = req.params.id;
-            noteArray.splice(deletedNote, 1);
-            for (let i = deletedNote; i < noteArray.length; i++) {
-                noteArray[i].id--;
-            }
-            fs.writeFile('db/db.json', JSON.stringify(noteArray), (err, data) => {
-                if (err) throw err;
-                console.log("note deleted")
-                res.send(data);
-            });
+        console.log("string")
+        // fs.readFile('db/db.json', 'utf8', function (err, data) {
+        //     if (err) throw err;
+        // const noteArray= JSON.parse(data);
+        // let deleteNote = req.params.id-1
+        // noteArray.splice(deleteNote, 1)
+        // for (let i = deleteNote; i < noteArray.length; i++) {
+        //     noteArray[i].id--;
+        // }
+        //     fs.writeFile('db/db.json', JSON.stringify(noteArray), (err, data) => {
+        //         if (err) throw err;
+        //         console.log("note deleted")
+        //         res.send(data);
+        //     });
 
-        })
+        // })
     })
 }
 
